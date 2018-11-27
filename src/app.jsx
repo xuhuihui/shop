7a52cdbeabd6a136a5570fda32a41ce90a76ab80
xuhuihui <xuhuihui@startdt.com>
2018-11-27 20:03:02 +0800

    feat:配置webpack

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: xuhuihui
+ * @Date: 2018-11-20 20:59:38
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-11-27 16:36:58
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
+import './index.scss'
+
+ReactDOM.render(
+  <div>
+    <h1>wwwww</h1></div>,
+  document.getElementById('app')
+)
